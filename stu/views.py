7b294c2cc63b9eea099a9b643827f11de291926c
lvbaoxin <745458741@qq.com>
2021-01-04 15:31:54 +0800

    Django 分页器(paginator)

--- stu/views.py
+++ stu/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
+import math
 
 
 # Create your views here.
@@ -21,11 +22,27 @@ def register_view(request):
         return HttpResponse("注册失败")
 
 
-def show_view(request):
-    # 1.查询stu_student表中的所有数据
-    stus = Student.objects.all()
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-    return render(request, 'show.html', {'show': stus})
+
+def show_view(request):
+    # 获取当前页码数
+    num = request.GET.get('num', 1)
+    n = int(num)
+    # 1.查询stu_student表中的所有数据
+    stus = Student.objects.all()  # 获取所有的
+    # django 分页
+    pager = Paginator(stus, 2)
+    # 获取当前页面的数据
+    try:
+        perpage_data = pager.page(n)
+        # 返回第一页的数据
+    except PageNotAnInteger:
+        perpage_data = pager.page(1)
+        # 返回最后一页的数据
+    except EmptyPage:
+        perpage_data = pager.page(pager.num_pages)
+    return render(request, 'show.html', {'show': stus, 'pager': pager, 'perpage_data': perpage_data})
 
 
 def login_view(request):
@@ -37,7 +54,7 @@ def login_view(request):
         password = request.POST.get("password", "")
         # 查询数据库
         if username and password:
-            c = Student.objects.filter(username = username, password = password).count()
+            c = Student.objects.filter(username=username, password=password).count()
 
         # 判断是否登录成功
         if c == 1:
@@ -50,7 +67,39 @@ def index_view(request):
 
 
 def movie_view(request):
-    movie = Movie.objects.all()
+    # movie = Movie.objects.all() 获取所有的
+    # 接收请求参数 num
+    # 原生分页
+    num = request.GET.get('num', 1)
+    # 处理分页请求
+    movies, n = page(num)
+    # 分页
+    # 页码 num 每页显示记录数size
+    #     1     2   [0:2]
+    #     2     2   [2:4]
+    #     3     2   [4:6]
+    #     4     2   [6:8]
+    #     num   size     [((num - 1) * size):(num * size)]
+    # 上一页的页码
+    pre_page_num = n - 1
+    # 下一页的页码
+    next_page_num = n + 1
+    return render(request, 'movie.html',
+                  {'movie': movies, 'pre_page_num': pre_page_num, 'next_page_num': next_page_num})
 
 
-    return render(request, 'movie.html', {'movie': movie})
+def page(num, size=2):
+    # 接收当前页面码
+    num = int(num)
+    # 总记录数
+    totalRecords = Movie.objects.count()
+    # 判断是否越界
+    if num < 1:
+        num = 1
+    totalPages = int(math.ceil(totalRecords * 1.0 / size))
+    # 取整
+    if num > totalPages:
+        num = totalPages
+    # 计算出每页显示的记录
+    movies = Movie.objects.all()[((num - 1) * size):(num * size)]
+    return movies, num
